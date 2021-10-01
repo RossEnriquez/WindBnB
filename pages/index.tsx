82ddfa5a948386f9ee46f0479da810b9ee5e5a2a
Ross Enriquez <enriquez.ross091@gmail.com>
2021-09-30 21:23:34 -0400

    Added search bar pop out, work in progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,21 +9,27 @@ import { AspectRatio, Button } from "@chakra-ui/react"
 import { elementDragControls } from "framer-motion/types/gestures/drag/VisualElementDragControls";
 
 const IndexPage = () => {
-  const [showSearch, setShowSearch] = useState(false);
+  const [showSearch, setShowSearch] = useState("none");
   const showSuper = false;
+  const toolBarProps = {
+    h: "100%",
+    w: "100%",
+    borderRadius: "15px"
+    
+  }
   var superHost, num_beds;
 
   const cities=stays.map((element, i) =>{
     superHost = null;
     num_beds = null;
     if (element.superHost)
-      superHost = <Box border="1px solid black" borderRadius={15} p="5px" mr="10px">{element.superHost==true && "Super host"}</Box>
+      superHost = <Box border="1px solid black" borderRadius={12} p="2px 5px" mr="10px">{element.superHost==true && "Super host"}</Box>
     if (element.beds != null)
       num_beds = <Text>: {`${element.beds} beds`}</Text>;
 
 
     return(
-      <Box key={i} display="inline-block" bg="red" w="500px" m={5}>
+      <Box key={i} display="inline-block" w="500px" m={5}>
         <Box w="100%" h="70%">
           <AspectRatio maxW="100%" ratio={3/2}>
             <Image objectFit="fill" src={element.photo} w="100%" h="100%" borderRadius="20px"></Image>
@@ -31,7 +37,7 @@ const IndexPage = () => {
         </Box>
         <Box>
           <Flex justifyContent="space-between" mt={3}>
-            <Flex>
+            <Flex h="30px">
               {superHost}
               <Text>{`${element.type}`}</Text>
               {num_beds}
@@ -50,18 +56,53 @@ const IndexPage = () => {
 
   return(
     <>
-    {showSearch && (
-          <Box>
+    <Box 
+      w="100%" 
+      h="100%" 
+      // display={showSearch}
+      display="flex" 
+      position="fixed"
+      top="0"
+      left="0"
+      zIndex="2"
+      bg="black"
+      opacity="40%"
+      onClick={() => setShowSearch("none")}
+    >
+    </Box>
+    <Box 
+      h="45%" 
+      // display={showSearch} 
+      p="0 10%"
+      pt="3%"
+      display="flex"
+      position="fixed"
+      w="100%"
+      top="0"
+      left="0"
+      zIndex="2"
+      opacity="100%"
+      bg="white"
+    >
+      <Center justifyContent="space-between" border="1px solid black" w="100%" h="15%">
+        <Button {...toolBarProps}>Location</Button>
+        <Button {...toolBarProps}>Guests</Button>
+        <Button {...toolBarProps}>Search</Button>
+      </Center>
+    </Box>
+    
+    {/* {showSearch && (
+          <Center position="fixed" >
             <Button onClick={() => setShowSearch(true)}>Search!</Button>
-          </Box>
-    )}
-    <Box id="header" bg="grey" p={50} h="10vh">
+          </Center>
+    )} */}
+    <Box id="header" p={50} h="10vh">
       <Center justifyContent="space-between" h="100%">
         <Box>
           <Image w="100%" h="100%" src={logoImage.src}></Image>
         </Box>
         <Flex 
-          onclick={()=> setShowSearch(true)} 
+          onClick={()=> setShowSearch("flex")} 
           _hover={{cursor: "pointer"}} 
           // border="1px solid black" 
           shadow="md"
@@ -77,7 +118,12 @@ const IndexPage = () => {
       </Center>
     </Box>
 
-    <Center id="body" m="0 5%" bg="blue" flexWrap="wrap">
+    <Center justifyContent="space-between" ml="9%" mr="9%" mt="3%">
+      <Heading>Stays in Finland</Heading>
+      <Text>Stays</Text>
+    </Center>
+    
+    <Center id="body" m="0 5%" flexWrap="wrap">
       {cities}
     </Center>
     </>
